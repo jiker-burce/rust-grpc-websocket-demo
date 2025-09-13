f2d91dcd8d3d9c8062d8745f3dde5cea8cd3ef5e
顾金鹤 <gujinhe@yousails.com>
2025-09-14 00:24:38 +0800

    添加各种room sql,解决存储数据时外键不存在问题: 添加执行shell命令

--- backend/migrations/002_add_rooms.sql
+++ backend/migrations/002_add_rooms.sql
@@ -7,3 +7,6 @@ ON DUPLICATE KEY UPDATE name = name;
 INSERT INTO rooms (id, name, description, is_public, created_by) 
 VALUES ('random', '闲聊群', '闲聊群，随意聊天和讨论', TRUE, 'system')
 ON DUPLICATE KEY UPDATE name = name;
+
+-- 执行脚本
+-- mysql -u chat_user -pchat_password -h localhost chat_db < migrations/002_add_rooms.sql
